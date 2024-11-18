0f4b4e28389f05fafc75f4c1964244b6dccb9cb3
sreerajpr33 <prsreeraj33@gmail.com>
2024-11-18 11:33:21 +0530

    django

--- fresh/app/urls.py
+++ fresh/app/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views
 from django.urls import path,include
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 
 urlpatterns=[
@@ -10,7 +9,7 @@ urlpatterns=[
     path('shop_home',views.shop_home),
     path('logout',views.freash_logout),
     path('add_product',views.addproduct),
+    path('editproduct/<pid>',views.editproduct),
 ]
 
-if settings.DEBUG:
-    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
--- fresh/app/views.py
+++ fresh/app/views.py
@@ -54,3 +54,24 @@ def addproduct(req):
             return render(req,'shop/product.html')
     else:
         return redirect(fresh_login)
+
+def editproduct(req,pid):
+    if req.method=='POST':
+        p_id=req.POST['pid']
+        name=req.POST['name']
+        dis=req.POST['dis']
+        price=req.POST['price']
+        off_p=req.POST['off_p']
+        stock=req.POST['stock']
+        file=req.FILES.get('img')
+        if file:
+            product.objects.filter(pk=pid).update(pid=p_id,name=name,dis=dis,price=price,offer_price=off_p,stock=stock)
+            data=product.objects.get(pk=pid)
+            data.img=file
+            data.save()
+        else:
+            product.objects.filter(pk=pid).update(pid=p_id,name=name,dis=dis,price=price,offer_price=off_p,stock=stock)
+        return redirect(shop_home)
+    else:
+        data=product.objects.get(pk=pid)
+        return render(req,'shop/edit.html',{'data':data})
